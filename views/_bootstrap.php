02d4228431aad2043d7780cc003e9526141a951d
teiGustavo <gteixeiradesousa7@gmail.com>
2024-06-23 03:22:58 -0300

    :sparkles: feat: Melhoria nos retornos para o usuário a depender da autenticação

--- views/_bootstrap.php
+++ views/_bootstrap.php
@@ -41,6 +41,8 @@
                         <a class="nav-link" href="<?= $router->route('spreadsheet.index'); ?>">Gerar Relatório</a>
 
                         <?php if (NEEDS_AUTH === 'true') : ?>
+                            <a class="nav-link" href="<?= $router->route('admin'); ?>">Administração</a>
+
                             <a class="nav-link" href="<?= $router->route('auth.logout'); ?>">Sair</a>
                         <?php endif; ?>
 
--- views/home.php
+++ views/home.php
@@ -2,4 +2,15 @@
 $this->layout("_bootstrap", ["title" => $title]);
 ?>
 
-<h1 class="fs-6">Seja bem vindo(a) ao sistema!</h1>
+<div>
+    <?php if (NEEDS_AUTH === 'true') : ?>
+
+        <h1 class="fs-5">Olá usuário(a),</h1>
+        <h2 class="fs-6">Seja bem vindo(a) ao sistema!</h2>
+
+    <?php else : ?>
+
+        <h1 class="fs-5">Seja bem vindo(a) ao sistema!</h1>
+
+    <?php endif; ?>
+</div>
